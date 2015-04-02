626f59f3b7c444856376f98ed2b7745bcbea35fd
sofus <sofusalbertsen@gmail.com>
2015-04-02 18:49:35 +0200

    Added comments and moved to right folder

---
+++ public_html/js/test.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active"); //Find all other in the list, and remove the acive class
+        $(this).addClass("active"); //and add it on this one.
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
