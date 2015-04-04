8275071310750fdf4a9cbf5c02c9b2fb56033bc1
sofus <sofusalbertsen@gmail.com>
2015-04-04 15:28:58 +0200

    added geocoder for the lendout.js

--- public_html/js/borrow.js
+++ public_html/js/borrow.js
@@ -1,6 +1,8 @@
 //dummy because we do not have user data to place
-var dummycoordinates=[{lat:55.6943541,lng:12.5486899},{lat:55.6995298,lng:12.5767136},{lat:55.6819198,lng:12.5555563},{lat:55.6786896,lng:12.5677443},{lat:55.6710063,lng:12.5703621},{lat:55.6764755,lng:12.509079},{lat:55.7034400,lng:12.5397635},{lat:55.6890567,lng:12.5321675}];
-var markers=[];
+var dummycoordinates = [{lat: 55.6943541, lng: 12.5486899}, {lat: 55.6995298, lng: 12.5767136}, {lat: 55.6819198, lng: 12.5555563}, {lat: 55.6786896, lng: 12.5677443}, {lat: 55.6710063, lng: 12.5703621}, {lat: 55.6764755, lng: 12.509079}, {lat: 55.7034400, lng: 12.5397635}, {lat: 55.6890567, lng: 12.5321675}];
+var markers = [];
+var geocoder;
+var map;
 $(document).ready(function () {
     $("#MainCat>li>a").click(function (e) {
         e.preventDefault();
@@ -18,20 +20,20 @@ $(document).ready(function () {
         console.log(placeholders);
         $('.picture-list').children('img').remove();
         setAllMap(null);
-        markers=[];
+        markers = [];
         for (var counter = 0; counter < placeholders; counter++) {
             //Display dummy images. Here we would need to get info from the server about images etc.
             $('.picture-list').append('<img src="img/175x175.gif" alt=""/>');
-           //Where can i borrow this imaged thing from? Marker is set here
-            addMarker(dummycoordinates[Math.floor((Math.random() * 6))],'hello World');
-            
+            //Where can i borrow this imaged thing from? Marker is set here
+            addMarker(dummycoordinates[Math.floor((Math.random() * 6))], 'hello World');
+
         }
     });
 
-    var map;
     initialize();
 //Google code to initialize the map.
     function initialize() {
+        geocoder = new google.maps.Geocoder();
         var mapOptions = {
             zoom: 12,
             center: new google.maps.LatLng(55.7034475, 12.5397635) //copenhagen
@@ -41,18 +43,18 @@ $(document).ready(function () {
         google.maps.event.addDomListener(window, 'load', initialize);
     }
     // Add a marker to the map and push to the array.
-function addMarker(location,text) {
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    title:text
-  });
-  markers.push(marker);
-}
+    function addMarker(location, text) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            title: text
+        });
+        markers.push(marker);
+    }
 // Sets the map on all markers in the array.
-function setAllMap(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
+    function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
 });
